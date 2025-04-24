dc379b66cfcf5ddb03347be177e62f76b182196a
Finn-Lunas <cenitelllll@gmail.com>
2025-04-24 17:33:33 +0300

    Fixed missing useState import in Equation2and6

--- src/components/Equation2and6.js
+++ src/components/Equation2and6.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function Equation2and6({ title }) {
